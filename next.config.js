4fa7a0100b6d65a987484754d6b5909fa11c3b54
xiaoshuangsu <a@adeMacBook-Air.local>
2026-02-12 03:14:43 -0800

    Fix: Remove assetPrefix to fix audio file paths

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/dictation-shadowing-tool',
-  assetPrefix: 'https://xiaoshuangsu.github.io/dictation-shadowing-tool/',
   output: 'export',
   images: {
     unoptimized: true,
